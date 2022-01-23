85751fb6e5118b0ea0b3f4d191658d78f7cb59ec
0xfaer <98242429+0xfaer@users.noreply.github.com>
2022-01-23 20:16:31 +1300

    Create includes.h

---
+++ CSGOExternal/includes/includes.h
@@ -0,0 +1,14 @@
+#include "../structs.h"
+#include "../EncStr.h"
+#include "../offsets.h"
+
+#define MAX_NAME 32
+
+typedef struct TempPlayerBase_t {
+    int h;
+    char szBasePlayerName[MAX_NAME];
+} TempPlayerBase;
+
+/*
+    Feel free to finish this, I really do not care for this.
+*/
